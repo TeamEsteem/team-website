e2ac9a4afeb6c97225a12caff1dc26d46759e375
mk210 <55575735+mk210@users.noreply.github.com>
2021-02-13 19:14:30 +0400

    added all the fonts!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import logo from './logo.svg';
-import {motion} from 'framer-motion';
-import './App.scss';
+import Landing from "./pages/landing";
+import "./App.scss";
+import { createMuiTheme, ThemeProvider, Typography } from "@material-ui/core";
+import Omnes from "./assets/fonts/Omnes Semibold Regular.ttf";
+
+const primaryFont = createMuiTheme({
+  typography: {
+    fontFamily: ["Inter", "sans-serif"].join(","),
+  },
+});
+
+const secondaryFont = createMuiTheme({
+  typography: {
+    fontFamily: "Omnes Semibold, sans-serif",
+  },
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        "@font-face": [Omnes],
+      },
+    },
+  },
+});
 
 function App() {
   return (
-    <div>Hello world!</div>
+    <div
+      style={{
+        background: "linear-gradient(90deg, #0BA360 0%, #3CBA92 100%)",
+        padding: "53px",
+        display: "flex",
+      }}
+    >
+      <ThemeProvider theme={secondaryFont}>
+        <Typography style={{ fontSize: "20px", fontWeight: "500" }}>
+          <Landing />
+        </Typography>
+      </ThemeProvider>
+    </div>
   );
 }
- 
+
 export default App;
- 
---
+++ src/pages/landing.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div>
+        {/* <img src={Logo} alt="logos" width="70%"></img> */}
+        <p style={{ float: "right" }}>The Landing Page!</p>
+      </div>
+    );
+  }
+}
+
+export default Landing;
