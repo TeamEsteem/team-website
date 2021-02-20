9681a21cba9190801531a08309090c05155b0797
mk210 <55575735+mk210@users.noreply.github.com>
2021-02-20 18:20:05 +0400

    Edited landing page.

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import Landing from "./pages/landing";
 import "./App.scss";
 import { ThemeProvider, Typography } from "@material-ui/core";
 import { primaryFont, secondaryFont } from "./components/fonts";
-
 function App() {
   return (
     <div
       style={{
-        background: "linear-gradient(90deg, #0BA360 0%, #3CBA92 100%)",
-        padding: "53px",
-        display: "flex",
+        // background: "linear-gradient(90deg, #0BA360 0%, #3CBA92 100%)",
+        position: "relative",
       }}
     >
-      <ThemeProvider theme={primaryFont}>
-        <Typography style={{ fontSize: "20px", fontWeight: "lighter" }}>
-          <Landing />
-        </Typography>
-      </ThemeProvider>
+      <div className="App-header">
+        <ThemeProvider theme={secondaryFont}>
+          <Typography style={{ fontSize: "20px", fontWeight: "lighter" }}>
+            <Landing />
+          </Typography>
+        </ThemeProvider>
+      </div>
     </div>
   );
 }
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -8,12 +8,86 @@ import {
   darkGrey,
   black,
 } from "../components/colors";
+import EsteemLogo from "../assets/images/logos.svg";
+import SDMELogo from "../assets/images/SDMELogo.svg";
+import {
+  Grid,
+  Typography,
+  Box,
+  ThemeProvider,
+  Button,
+} from "@material-ui/core";
+import { primaryFont } from "../components/fonts";
 class Landing extends Component {
   render() {
     return (
-      <div>
-        {/* <img src={Logo} alt="logos" width="70%"></img> */}
-        <p style={{ float: "right", color: white }}>The Landing Page!</p>
+      <div style={{ margin: "auto" }}>
+        {/* <Box m={2}></Box> */}
+        <Grid
+          container
+          alignContent="center"
+          direction="column"
+          justify="center"
+          spacing={1}
+        >
+          <Grid item>
+            <img
+              src={EsteemLogo}
+              alt="logos"
+              width="130em"
+              style={{
+                alignContent: "center",
+                display: "block",
+                marginLeft: "auto",
+                marginRight: "auto",
+                position: "relative",
+              }}
+            ></img>
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="h1"
+              style={{ textAlign: "center", color: esteemGreen }}
+            >
+              The Landing<br></br> <Box m={-2}></Box>page.
+            </Typography>
+          </Grid>
+          <Box m={2}></Box>
+          <Grid item>
+            <ThemeProvider theme={primaryFont}>
+              <Typography
+                variant="body1"
+                style={{
+                  paddingLeft: "100px",
+                  paddingRight: "100px",
+                  color: "white",
+                  fontWeight: "lighter",
+                  fontSize: "20px",
+                  textAlign: "center",
+                  margin: "auto",
+                  width: "50%",
+                }}
+              >
+                This is where we add the subtitle, a small description of the
+                team, or maybe something about the project.
+              </Typography>
+            </ThemeProvider>
+          </Grid>
+          <Box m={3} />
+          <Button
+            variant="outlined"
+            size="large"
+            style={{
+              width: "20%",
+              color: white,
+              margin: "auto",
+              border: "3px solid" + esteemGreen,
+            }}
+          >
+            Call to action
+          </Button>
+        </Grid>
+        {/* <Box m={-1} /> */}
       </div>
     );
   }
