385bdeb952ede85dd5ccab9518120cab55af40ca
Pranav C <30704120+PC-02@users.noreply.github.com>
2021-04-19 18:57:31 +0400

    added navbar to about

--- src/pages/team/teamV2.jsx
+++ src/pages/team/teamV2.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { AnimateSharedLayout, AnimatePresence } from "framer-motion";
-// import { Header } from "./Header";
+import Header from "../nav-barV2";
 import { Item } from "./Item";
 import { List } from "./List";
+import { Typography } from "@material-ui/core";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function Store({ match }) {
@@ -22,6 +23,11 @@ function Store({ match }) {
 export default function App() {
   return (
     <div className="container">
+      <div>
+        <Typography style={{ fontSize: "20px", fontWeight: "lighter" }}>
+          <Header />
+        </Typography>
+      </div>
       <AnimateSharedLayout type="crossfade">
         {/* <Header /> */}
         <Router>
