793f69026872f3963c26a70cc2b8b9dacb886040
Pranav C <30704120+PC-02@users.noreply.github.com>
2021-04-30 18:41:11 +0400

    demo impact page

--- src/pages/impact/impact.jsx
+++ src/pages/impact/impact.jsx
@@ -8,25 +8,65 @@ import {
   ThemeProvider,
   Button,
 } from "@material-ui/core";
+import {
+  white,
+  grey,
+  lightGreen,
+  esteemGreen,
+  darkGreen,
+  darkGrey,
+  black,
+} from "../../components/colors";
+import { primaryFont, secondaryFont } from "../../components/fonts";
+
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+
+class StaggeredList extends React.Component {
+    getItems() {
+        return ["Sustainability","Future","Innovation"," Clean Energy","Mobility","Smart Solutions","Happiness"]
+    }
+    
+    render() {
+        const items = this.getItems();
+        
+        return (
+            <div>
+                <ul className="list">
+                    <ReactCSSTransitionGroup transitionName="slide-up" transitionAppear={true}>
+                        {items.map((item, i) => {
+                            return <li key={i} className="pillar-item" style={{"transitionDelay": `${ i * .05 }s` }}>{item}</li>;
+                        })}
+                    </ReactCSSTransitionGroup>
+                </ul>
+            </div>
+        );
+    }
+}
+
+
 
 export default class Impact extends Component {
   render() {
     return (
+
       <div className="impact">
         <div>
           <Typography style={{ fontSize: "20px", fontWeight: "lighter" }}>
             <Header />
           </Typography>
         </div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <div className="impact-body">
-            Test
-        </div>
+        <div className="impact-head">
+        <ThemeProvider theme={secondaryFont}>
+              <Typography
+                variant="h2"
+                style={{ textAlign: "center", color: esteemGreen }}
+              >
+               THE PILLARS OF ESTEEM
+              </Typography>
+            </ThemeProvider>
+            </div>
+        <StaggeredList />
       </div>
     );
   }
