232213bc41803f12ef94febec729ce1399416856
alakbarz <me@alak.bar>
2021-05-06 00:46:29 +0100

    Added more members data and images

--- src/pages/team/members.jsx
+++ src/pages/team/members.jsx
@@ -1,50 +1,46 @@
 export const members = [
   // Marketing
   {
-    // id: "jairis",
     team: ["marketing", "architecture"],
     name: "Jairis",
-    role: "Marketing Team Lead",
-    image:
-      "https://images.unsplash.com/photo-1612492175848-3af2f7e8dd57?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    role: "Marketing Team Lead & Render Designer",
+    image:"/members/jairis.jpg",
   },
   {
-    team: ["marketing", "website"],
+    team: ["marketing", "website", "sensors"],
     name: "Alakbar",
     role: "Audio/Visual & Website Team Lead",
-    image:
-      "https://images.unsplash.com/photo-1612492175848-3af2f7e8dd57?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    image:"/members/alakbar.jpg",
+  },
+  {
+    team: ["marketing", "building_services"],
+    name: "Adam",
+    role: "Videographer & Radiant Cooling System Technician",
+    image:"/members/adam.jpg",
+  },
+  {
+    team: ["renewables"],
+    name: "Jonathan",
+    role: "Content Creator & Writer",
+    image:"/members/jonathan_naftalin.jpg",
   },
   {
     team: ["marketing"],
     name: "Pietro",
     role: "Marketing Assisstant & Content Creator",
+    image:"/members/pietro.jpg",
   },
   {
     team: ["marketing"],
     name: "Yashodhara",
     role: "Event Coordinator",
+    image:"/members/yashodhara.jpg",
   },
   {
-    team: ["marketing"],
+    team: ["marketing", "architecture"],
     name: "Sonia B.",
     role: "Podcast Lead/Host",
-  },
-  {
-    team: ["marketing"],
-    name: "Ruschane",
-    role: "Podcast Lead/Host & Content Creator",
-  },
-  {
-    team: ["marketing"],
-    name: "Eshani",
-    role: "Podcast Content Creator & Director",
-  },
-
-  {
-    team: ["marketing"],
-    name: "Eshani",
-    role: "Podcast Content Creator & Director",
+    image:"/members/sonia_b.jpg",
   },
 
   // Website
@@ -58,4 +54,484 @@ export const members = [
     name: "Pranav",
     role: "Website Developer",
   },
+
+  // Software & Sensors
+  {
+    team: ["sensors"],
+    name: "Elliot",
+    role: "Software & Sensors Team Lead",
+    image:"/members/elliot_whitehouse.jpg",
+  },
+  {
+    team: ["sensors", "sponsorship", "landscaping", "architecture", "windcatcher"],
+    name: "Scott",
+    role: "Sponsorship & Procurement",
+    image:"/members/scott.jpg",
+  },
+  {
+    team: ["sensors"],
+    name: "Lukasz",
+    role: "ARBNCO Sensors Lead",
+    image:"/members/lukasz.jpg",
+  },
+  {
+    team: ["sensors"],
+    name: "Ghazi",
+    role: "AR/AI Lead",
+    image:"/members/ghazi.jpg",
+  },
+  {
+    team: ["sensors"],
+    name: "Oluwasanmi",
+    role: "AR Developer",
+    image:"/members/oluwasanmi.jpg",
+  },
+  {
+    team: ["sensors"],
+    name: "Vishnu",
+    role: "Alexa Lead",
+    image:"/members/vishnu.jpg",
+  },
+  {
+    team: ["sensors"],
+    name: "Petar",
+    role: "OpenHAB Lead",
+    image:"/members/petar.jpg",
+  },
+  {
+    team: ["sensors"],
+    name: "Navya",
+    role: "Smart Screen Developer",
+    image:"/members/navya.jpg",
+  },
+  {
+    team: ["sensors"],
+    name: "Geraldine",
+    role: "Systems Lead",
+    image:"/members/geraldine.jpg",
+  },
+
+  // Sponsorship
+  {
+    team: ["sponsorship"],
+    name: "Fatima",
+    role: "Sponsorship Researcher",
+    image:"/members/fatima.jpg",
+  },
+  {
+    team: ["sponsorship"],
+    name: "Ajoke",
+    role: "Sponsorship Researcher",
+    image:"/members/ajoke.jpg",
+  },
+
+  // Landscaping
+  {
+    team: ["landscaping"],
+    name: "Madihah",
+    role: "Plant & Vegetation Coordinator",
+    image:"/members/madihah_anaum.jpg",
+  },
+  {
+    team: ["landscaping", "newsletter"],
+    name: "Ayman",
+    role: "Landscaping Team Member",
+    image:"/members/ayman.jpg",
+  },
+  {
+    team: ["landscaping", "windcatcher"],
+    name: "Joe",
+    role: "Landscaping Team Lead",
+    image:"/members/joe.jpg",
+  },
+  
+  // Renewables
+  {
+    team: ["renewables"],
+    name: "Kelvin",
+    role: "Renewables Team Lead",
+    image:"/members/kelvin.jpg",
+  },
+  {
+    team: ["renewables"],
+    name: "Marta",
+    role: "Renewables Team Lead",
+    image:"/members/marta.jpg",
+  },
+  {
+    team: ["renewables"],
+    name: "Alexander",
+    role: "Solar Systems Engineer",
+    image:"/members/alexander.jpg",
+  },
+  {
+    team: ["renewables"],
+    name: "Elisa",
+    role: "Cables Management & Equipment's Internal Head Gains",
+    image:"/members/elisa.jpg",
+  },
+  {
+    team: ["renewables", "building_services"],
+    name: "Luke C.",
+    role: "Mechanical Systems & Radiant Cooling System Designer",
+    image:"/members/luke_c.jpg",
+  },
+  {
+    team: ["renewables"],
+    name: "Luke O.",
+    role: "Electrical Systems",
+    image:"/members/luke_o.jpg",
+  },
+  {
+    team: ["renewables", "marketing"],
+    name: "Ruschane",
+    role: "Electromechanical Design & Podcast Lead/Host",
+    image:"/members/ruschane.jpg",
+  },
+  {
+    team: ["renewables", "marketing"],
+    name: "Eshani",
+    role: "Electromechanical Design & Podcast Director",
+    image:"/members/eshani_mhatre.jpg",
+  },
+  {
+    team: ["renewables"],
+    name: "Stephen",
+    role: "Energy Storage Systems Lead",
+    image:"/members/stephen.jpg",
+  },
+  {
+    team: ["renewables"],
+    name: "Czarina",
+    role: "Lighting Lead & Wiring Consultant",
+    image:"/members/czarina.jpg",
+  },
+  {
+    team: ["renewables"],
+    name: "Ahmed",
+    role: "PV Consultant",
+    image:"/members/ahmed.jpg",
+  },
+  {
+    team: ["renewables"],
+    name: "Tilda",
+    role: "SCADA & Sensors Lead",
+    image:"/members/tilda.jpg",
+  },
+  {
+    team: ["renewables"],
+    name: "Joao",
+    role: "SCADA & Sensors Lead",
+    image:"/members/joao.jpg",
+  },
+  {
+    team: ["renewables"],
+    name: "Shehan",
+    role: "PVT Modeller",
+    image:"/members/shehan.jpg",
+  },
+  {
+    team: ["renewables"],
+    name: "Willis",
+    role: "PVT Modeller",
+    image:"/members/willis_deans.jpg",
+  },
+  {
+    team: ["renewables"],
+    name: "Claudia",
+    role: "Rainwater Systems",
+    image:"/members/claudia.jpg",
+  },
+  
+  // Quantity Surveying
+  {
+    team: ["quantity"],
+    name: "Callum",
+    role: "Quantity Surveying Team Lead",
+    image:"/members/callum_lewington.jpg",
+  },
+  {
+    team: ["quantity"],
+    name: "Liam",
+    role: "Quantity Surveying Team Member",
+    image:"/members/liam_gordon.jpg",
+  },
+  
+  // Architecture
+  {
+    team: ["landscaping", "architecture"],
+    name: "Martin",
+    role: "Architecture Team Lead & Water Systems Engineer",
+    image:"/members/martinj.jpg",
+  },
+  {
+    team: ["architecture", "windcatcher"],
+    name: "Kinga",
+    role: "Interior Design Team Lead",
+    image:"/members/kinga.jpg",
+  },
+  {
+    team: ["architecture"],
+    name: "Alix",
+    role: "Architecture Team Member",
+    image:"/members/alix.jpg",
+  },
+  {
+    team: ["architecture"],
+    name: "Aurelia",
+    role: "Render Designer",
+    image:"/members/aurelia.jpg",
+  },
+  {
+    team: ["architecture"],
+    name: "Bailey",
+    role: "Architecture Team Member",
+    image:"/members/Bailey.jpg",
+  },
+  {
+    team: ["architecture"],
+    name: "Jil",
+    role: "Architecture Team Member",
+    image:"/members/jil_sheth.jpg",
+  },
+
+  // Structural
+  {
+    team: ["structural"],
+    name: "Ellie",
+    role: "Structural Team Lead",
+    image:"/members/ellie.jpg",
+  },
+  {
+    team: ["structural"],
+    name: "Lucas",
+    role: "Model Creator",
+    image:"/members/lucas.jpg",
+  },
+  {
+    team: ["structural"],
+    name: "Siddheshwar",
+    role: "Structural Team Member",
+    image:"/members/siddheshwar.jpg",
+  },
+  {
+    team: ["structural", "construction"],
+    name: "Chloe",
+    role: "Structural & Second Skin Team Member",
+    image:"/members/chloejc.jpg",
+  },
+  {
+    team: ["structural", "windcatcher"],
+    name: "Izabela",
+    role: "Structural Team Member",
+    image:"/members/izabela.jpg",
+  },
+  {
+    team: ["structural"],
+    name: "Laura",
+    role: "Structural Team Member",
+    image:"/members/laura.jpg",
+  },
+  {
+    team: ["structural"],
+    name: "Moustapha",
+    role: "Structural Team Member",
+    image:"/members/moustapha.jpg",
+  },
+  {
+    team: ["structural"],
+    name: "Joel",
+    role: "Structural Team Member",
+    image:"/members/joel.jpg",
+  },
+
+  // Health & Safety
+  {
+    team: ["health"],
+    name: "Zuhair",
+    role: "Health & Safety Team Lead",
+    image:"/members/zuhair.jpg",
+  },
+  {
+    team: ["health", "renewables"],
+    name: "Ryan",
+    role: "Health & Safety Co-Team Lead & Electrical Systems",
+    image:"/members/ryan.jpg",
+  },
+  {
+    team: ["health"],
+    name: "Aashni",
+    role: "Health & Safety Officer",
+    image:"/members/aashni.jpg",
+  },
+  {
+    team: ["health"],
+    name: "Abdulrahman",
+    role: "Health & Safety Officer",
+    image:"/members/abdulrahman.jpg",
+  },
+  {
+    team: ["health"],
+    name: "Oke",
+    role: "Health & Safety Officer",
+    image:"/members/oke.jpg",
+  },
+  {
+    team: ["health"],
+    name: "Priyanka",
+    role: "Health & Safety Officer",
+    image:"/members/priyanka.jpg",
+  },
+  {
+    team: ["health"],
+    name: "Deepangika",
+    role: "Health & Safety Officer",
+    image:"/members/deepangika.jpg",
+  },
+
+  // Building Services
+  {
+    team: ["building_services"],
+    name: "Gabriela",
+    role: "Water Management",
+    image:"/members/gabriela.jpg",
+  },
+  {
+    team: ["building_services"],
+    name: "Duncan",
+    role: "Ventilation Systems",
+    image:"/members/duncan.jpg",
+  },
+  {
+    team: ["building_services"],
+    name: "Bruno",
+    role: "Water Storage Lead",
+    image:"/members/bruno.jpg",
+  },
+
+    // Building Modelling
+    {
+      team: ["building_modelling"],
+      name: "Priyankadevi",
+      role: "IES & Building Modelling Team Lead",
+      image:"/members/priyankadevi.jpg",
+    },
+    {
+      team: ["building_modelling"],
+      name: "Mahnoor",
+      role: "IES Model",
+      image:"/members/mahnoor.jpg",
+    },
+    {
+      team: ["building_modelling"],
+      name: "Vinuki",
+      role: "IES Model",
+      image:"/members/vinuki.jpg",
+    },
+    {
+      team: ["building_modelling"],
+      name: "Nathan",
+      role: "Computational Fluid Dyanmics",
+      image:"/members/nathan.jpg",
+    },
+    {
+      team: ["building_modelling"],
+      name: "Jane",
+      role: "Revit Model",
+      image:"/members/jane.jpg",
+    },
+    {
+      team: ["building_modelling"],
+      name: "Emilia",
+      role: "Revit Model",
+      image:"/members/emilia.jpg",
+    },
+    {
+      team: ["building_modelling"],
+      name: "Serrah",
+      role: "AutoCAD & IES",
+      image:"/members/serrah.jpg",
+    },
+    {
+      team: ["building_modelling"],
+      name: "Eshiet",
+      role: "AutoCAD Model",
+      image:"/members/eshiet_akpan.jpg",
+    },
+
+    // Construction
+    {
+      team: ["construction", "windcatcher"],
+      name: "Jessica",
+      role: "Construction & Windcatcher Team Leads",
+      image:"/members/jessica_haskett.jpg",
+    },
+    {
+      team: ["construction"],
+      name: "Marco",
+      role: "Site Operations Lead",
+      image:"/members/marco.jpg",
+    },
+    {
+      team: ["construction"],
+      name: "Umma",
+      role: "Site Operations Coordinator",
+      image:"/members/umma.jpg",
+    },
+    {
+      team: ["construction"],
+      name: "Shilpan",
+      role: "Site Operations Coordinator",
+      image:"/members/shilpan.jpg",
+    },
+    {
+      team: ["construction"],
+      name: "Devarsi",
+      role: "Second Skin Team Member",
+      image:"/members/devasri.jpg",
+    },
+    {
+      team: ["construction"],
+      name: "Harsha",
+      role: "Logistics",
+      image:"/members/harsha.jpg",
+    },
+    {
+      team: ["construction"],
+      name: "David",
+      role: "Construction Detailing Lead",
+      image:"/members/david.jpg",
+    },
+    {
+      team: ["construction"],
+      name: "Ili",
+      role: "Construction Detailing",
+      image:"/members/ili.jpg",
+    },
+    {
+      team: ["construction"],
+      name: "Ahmed",
+      role: "Construction Detailing",
+      image:"/members/ahmed.jpg",
+    },
+
+    // Windcatcher
+    {
+      team: ["windcatcher"],
+      name: "Erik",
+      role: "Windcatcher Co-Team Lead",
+      image:"/members/erik.jpg",
+    },
+    {
+      team: ["windcatcher"],
+      name: "Carlos",
+      role: "Windcatcher Team Member",
+      image:"/members/carlosf.jpg",
+    },
+    {
+      team: ["windcatcher"],
+      name: "Pelle",
+      role: "Windcatcher Team Member",
+      image:"/members/pelle.jpg",
+    },
 ];
