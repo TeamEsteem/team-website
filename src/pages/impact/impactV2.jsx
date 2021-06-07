87817a502bd036b18f15e5856e3b7327f69b70ae
mk210 <55575735+mk210@users.noreply.github.com>
2021-06-07 20:11:54 +0400

    added dialog box for all the pillars.

--- src/pages/impact/impactV2.jsx
+++ src/pages/impact/impactV2.jsx
@@ -3,7 +3,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import ButtonBase from "@material-ui/core/ButtonBase";
 import { black, esteemGreen, white } from "../../components/colors";
 import Header from "../nav-barV2";
-import { Box, Grid, Typography, ThemeProvider } from "@material-ui/core";
+import {
+  Box,
+  Grid,
+  Typography,
+  ThemeProvider,
+  Divider,
+} from "@material-ui/core";
 import { primaryFont, secondaryFont } from "../../components/fonts";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
@@ -17,84 +23,56 @@ import { useTheme } from "@material-ui/core/styles";
 function ImpactButtonClick() {
   console.log("heylo");
 }
-function ImpactDialogueBox() {
-  const [open, setOpen] = React.useState(false);
-  const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  return (
-    <div>
-      <Dialog
-        fullScreen={fullScreen}
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="responsive-dialog-title"
-      >
-        <DialogTitle id="responsive-dialog-title">
-          {"Use Google's location service?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Let Google help apps determine location. This means sending
-            anonymous location data to Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose} color="primary">
-            Disagree
-          </Button>
-          <Button onClick={handleClose} color="primary" autoFocus>
-            Agree
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
-}
 
 const images = [
   {
+    key: 1,
     url: "https://images.unsplash.com/photo-1521708266372-b3547456cc2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1924&q=80",
     title: "Sustainability",
     width: "14.285714285714285714285714285714%",
+    desc: "Our house will be built from a broad range of environmentally friendly construction materials. From cross-laminated timber to  woodfibre insulation, every element of the design is designed to minimise energy and water consumption, helping to downsize the carbon footprint of the house occupants.",
   },
   {
+    key: 2,
     url: "https://images.pexels.com/photos/1152707/pexels-photo-1152707.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     title: "Future",
     width: "14.285714285714285714285714285714%",
+    desc: "With building services developing along several exciting technological frontiers, we are eager to incorporate as much futuristic tech into our design as possible. From voice activated appliances to a windcatcher made from 3D printed concrete, Team ESTEEM are not afraid to think outside the box when it comes to embracing the technology of tomorrow.",
   },
   {
+    key: 3,
     url: "https://images.pexels.com/photos/355952/pexels-photo-355952.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     title: "Innovation",
     width: "14.285714285714285714285714285714%",
+    desc: "At the heart of our home lies our steadfast commitment to innovation in architectural design. Through our collaboration with industry and academic partners, we have equipped our building with cutting-edge renewable energy technology such as bifacial solar panels and bespoke water heating systems. ",
   },
   {
+    key: 4,
     url: "https://images.pexels.com/photos/3976320/pexels-photo-3976320.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     title: "Clean Energy",
     width: "14.285714285714285714285714285714%",
+    desc: "Our dedicated renewables sub-team have worked tirelessly to develop the electrical generation systems that will power our competition build. Photovoltaic panels and solar-thermal devices are expertly woven into the design of the house, ensuring that our submission achieves the critical net zero carbon output. ",
   },
   {
+    key: 5,
     url: "https://images.unsplash.com/photo-1598958574774-a38471f4f216?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
     title: "Mobility",
-    width: "14.285714285714285714285714285714%",
+    width: "20.285714285714285714285714285714%",
+    desc: "Our home has been designed to facilitate sustainable travel using electric vehicles, an essential feature of any nation’s efforts to achieve decarbonisation. Our house itself is characterised by its mobility, designed for rapid assembly and disassembly with a minimal resultant impact on its structural integrity. ",
   },
   {
+    key: 6,
     url: "https://images.unsplash.com/photo-1539627831859-a911cf04d3cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80",
     title: "Smart Solutions",
     width: "14.285714285714285714285714285714%",
+    desc: "Fit with an intelligent and intuitive energy management system that allows users to fine-tune essential parameters of the interior environment, our house exemplifies “smart”. Interactive panels and voice-controlled systems provide a high-tech solution to the question of customisability, maximising user comfort in the home.",
   },
   {
+    key: 7,
     url: "https://images.pexels.com/photos/2861525/pexels-photo-2861525.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     title: "Happiness",
     width: "14.285714285714285714285714285714%",
+    desc: "Most importantly, our house is designed to be a home. Our blend of technology and artistic interior design ensures the user experience is at the heart of our build, with each room, feature and space designed for holistic integration with the Team ESTEEM ethos of comfort and homeliness.",
   },
 ];
 
@@ -119,6 +97,7 @@ const useStyles = makeStyles((theme) => ({
       },
       "& $imageMarked": {
         opacity: 0,
+        filter: "grayscale(20%)",
       },
       "& $imageTitle": {
         border: "4px solid currentColor",
@@ -174,8 +153,20 @@ const useStyles = makeStyles((theme) => ({
     transition: theme.transitions.create("opacity"),
   },
 }));
+
 function ImpactButtons() {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  // const theme = useTheme();
+  // handleClickOpen();
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div className={classes.root}>
@@ -183,11 +174,14 @@ function ImpactButtons() {
         <ButtonBase
           focusRipple
           key={image.title}
-          onClick={ImpactDialogueBox.handleClickOpen}
+          onClick={handleClickOpen}
           className={classes.image}
           focusVisibleClassName={classes.focusVisible}
           style={{
             width: image.width,
+            display: "flex",
+            margin: "auto",
+            // width: "100%",
           }}
         >
           <span
@@ -213,10 +207,114 @@ function ImpactButtons() {
           </span>
         </ButtonBase>
       ))}
+      <div>
+        {/* <Button onClick={handleClickOpen}>Hello</Button> */}
+        {images.map((image) => {
+          if (image.key === 2) {
+            return (
+              <Dialog
+                // fullScreen={fullScreen}
+                open={open}
+                key={image.key}
+                onClose={handleClose}
+                aria-labelledby="responsive-dialog-title"
+              >
+                <DialogTitle id="responsive-dialog-title">
+                  {image.title}
+                </DialogTitle>
+                <Divider />
+
+                <DialogContent>
+                  <DialogContentText>{image.desc}</DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose} autoFocus>
+                    CLOSE
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            );
+          }
+        })}
+      </div>
     </div>
   );
 }
 
+function ImpactButtonsV2(props) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  // const theme = useTheme();
+  // handleClickOpen();
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <ButtonBase
+        focusRipple
+        key={props.title}
+        onClick={handleClickOpen}
+        className={classes.image}
+        focusVisibleClassName={classes.focusVisible}
+        style={{
+          width: props.width,
+          // width: "100%",
+        }}
+      >
+        <span
+          className={classes.imageSrc}
+          style={{
+            backgroundImage: `url(${props.url})`,
+          }}
+        />
+        <span className={classes.imageBackdrop} />
+        <span className={classes.imageButton}>
+          <ThemeProvider theme={primaryFont}>
+            <Typography
+              component="span"
+              variant="body1"
+              // style={{ fontSize: "20px" }}
+              color="inherit"
+              className={classes.imageTitle}
+            >
+              {props.title}
+              <span className={classes.imageMarked} />
+            </Typography>
+          </ThemeProvider>
+        </span>
+      </ButtonBase>
+      <div>
+        <Dialog
+          // fullScreen={fullScreen}
+          open={open}
+          key={props.key}
+          onClose={handleClose}
+          aria-labelledby="responsive-dialog-title"
+        >
+          <DialogTitle id="responsive-dialog-title">{props.title}</DialogTitle>
+          <Divider />
+
+          <DialogContent>
+            <DialogContentText>{props.desc}</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} autoFocus>
+              CLOSE
+            </Button>
+          </DialogActions>
+        </Dialog>
+        {/* ); */}
+      </div>
+    </div>
+  );
+}
 class ImpactV2 extends Component {
   render() {
     return (
@@ -290,20 +388,16 @@ class ImpactV2 extends Component {
             </ThemeProvider>
           </Grid>
         </Grid>
-        {/* <div class="landing-bottom" style={{ backgroundColor: esteemGreen }}>
-          <svg
-            data-name="Layer 1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1200 120"
-            preserveAspectRatio="none"
-          >
-            <path
-              d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-              style={{ fill: white }}
-            ></path>
-          </svg>
-        </div> */}
-        <ImpactButtons />
+        {images.map((image) => (
+          <ImpactButtonsV2
+            // key={image.key}
+            url={image.url}
+            title={image.title}
+            width={image.width}
+            desc={image.desc}
+          />
+        ))}
+
         <div style={{ margin: "2px" }}></div>
       </Box>
     );
