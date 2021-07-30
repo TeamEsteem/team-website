6dfda1e2acd784609b66099216c32614c4e68c88
PC-02 <pranavc10@hotmail.com>
2021-07-30 23:19:14 +0400

    impact color changes

--- src/pages/about-home/home-modelV2.jsx
+++ src/pages/about-home/home-modelV2.jsx
@@ -408,21 +408,21 @@ class HomeModelV2 extends Component {
             </Grid>
           </Grid>
         </div>
-        <Box m={10}></Box>
-        <div class="landing-bottom" id="landingBottom">
-          <svg
-            data-name="Layer 1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1100 120"
-            preserveAspectRatio="none"
-          >
-            <path
-              d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-              class="shape-fill"
-            ></path>
-          </svg>
-        </div>
-        <div style={{ backgroundColor: "#fff", padding: "0 10px" }}>
+        <Box m={10}></Box>      
+        <div class="sections-bottom">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
+        <div style={{ backgroundColor: "#f5f5f5", padding: "0 10px" }}>
           <Grid container direction="column" style={{ maxWidth: "1250px" }}>
             <ThemeProvider theme={secondaryFont}>
               <Typography
@@ -472,6 +472,8 @@ class HomeModelV2 extends Component {
                 ))}
         <br>
         </br>
+        <br></br>
+        <br></br>
         <br></br>
         <div class="footer-top">
           <svg
